a389838514cb5c91d924ca6b6773d53f1f08a094
Asier Comba <asier.comba@opendeusto.es>
2026-03-09 17:31:47 +0100

    DEMO menu principal(prueba)

--- admin_local_c/main.c
+++ admin_local_c/main.c
@@ -9,6 +9,32 @@
 #include <stdio.h>
 
 int main(void) {
-    printf("Hola\n");
+    int opcion;
+
+    do {
+        printf("\n==============================\n");
+        printf("DEUSTO-AIR\n");
+        printf("==============================\n");
+        printf("1) Iniciar sesion\n");
+        printf("2) Registrar usuario\n");
+        printf("0) Salir\n");
+        printf("Opcion: ");
+        scanf("%d", &opcion);
+
+        switch (opcion) {
+            case 1:
+                printf("Has elegido iniciar sesion.\n");
+                break;
+            case 2:
+                printf("Has elegido registrar usuario.\n");
+                break;
+            case 0:
+                printf("Saliendo...\n");
+                break;
+            default:
+                printf("Opcion no valida.\n");
+        }
+    } while (opcion != 0);
+
     return 0;
 }
