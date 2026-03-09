f01910b2e2e8cd36a07e4d90f757d8e87fa70f79
Asier Comba <asier.comba@opendeusto.es>
2026-03-09 17:44:02 +0100

    Actualizacion de la demo del menu

--- admin_local_c/src/admin_local_c.c
+++ admin_local_c/src/admin_local_c.c
@@ -9,9 +9,35 @@
  */
 
 #include <stdio.h>
-#include <stdlib.h>
 
 int main(void) {
-	puts("!!!Hello World!!!"); /* prints !!!Hello World!!! */
-	return EXIT_SUCCESS;
+    int opcion;
+
+    do {
+        printf("\n==============================\n");
+        printf("DEUSTO-AIR\n");
+        printf("==============================\n");
+        printf("1) Iniciar sesion\n");
+        printf("2) Registrar usuario\n");
+        printf("0) Salir\n");
+        printf("Opcion: ");
+        scanf("%d", &opcion);
+
+        switch (opcion) {
+            case 1:
+                printf("Has elegido iniciar sesion.\n");
+                break;
+            case 2:
+                printf("Has elegido registrar usuario.\n");
+                break;
+            case 0:
+                printf("Saliendo...\n");
+                break;
+            default:
+                printf("Opcion no valida.\n");
+        }
+    } while (opcion != 0);
+
+    return 0;
 }
+
